83c15336586e8c4ce997f60f35f2cb495f457c36
Daniel Bastos <dsbastos1@hotmail.com>
2021-02-25 16:06:24 +0000

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { ChallengeBox } from '../components/ChallengeBox';
 
 
 import styles from '../styles/pages/Home.module.css'
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 
 export default function Home() {
@@ -20,17 +21,20 @@ export default function Home() {
       </Head>
 
       <ExperienceBar />
+      <CountdownProvider>
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+
     </div >
   )
 }
